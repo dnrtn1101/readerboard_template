749bf7a461f9bef8bee094db083c14e8eb0ddff4
ajouJSB <jsebin1327@ajou.ac.kr>
2021-05-12 23:33:35 +0900

    ans_to_cnn

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -11,7 +11,7 @@ def read_txt(fileName):
 
 
 def write_json(fileName, data):
-    with open(fileName, 'w', encoding='utf-8') as f:
+    with open(fileName, 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
@@ -33,13 +33,8 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    # 1.이메일을 통해서 전달 받은 키 파일의 경로 입력
-    key_path = "201720804.pem"
-    # 2. 예측한 결과를 텍스트 파일로 저장했을 경우 리스트로 다시 불러오기
-    # 본인이 원하는 방식으로 리스트 형태로 예측 값을 불러오기만 하면 됨(순서를 지킬것)
-    raw_ans_path = "ans.txt"
-    ans = read_txt(raw_ans_path)
-    # 3. 암호화된 파일을 저장할 위치
-    encrypt_ans_path = "../submission/201720804/ans.json"
-    # 4. 암호화!(pycrytodome 설치)
+    key_path = "201920707.pem"
+    raw_ans_path="ans.txt"
+    ans=read_txt(raw_ans_path)
+    encrypt_ans_path = "../submission/201920707/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
