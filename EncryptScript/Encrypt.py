1167711cd7a79e57bc09f67bdbdfd3be56e6861c
seunghun <xldi28@ajou.ac.kr>
2021-06-02 14:32:36 +0900

    test

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -32,9 +32,9 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
     write_json(encrypt_store_path, {'iv': iv, 'ciphertext': ct})
 
 if __name__ == "__main__":
-    key_path = "key.pem"
+    key_path = "201720809.pem"
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201723277/ans.json"
+    encrypt_ans_path = "../submission/201720809/ans.json"
 
     encrypt_data(key_path, ans, encrypt_ans_path)
