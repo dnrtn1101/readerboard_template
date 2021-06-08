ae8ef7ffeeb4688fd5f158b1a50b888d5e7f1051
고종환 <jonghwankoh@ajou.ac.kr>
2021-06-09 05:01:45 +0900

    FinalHW Sub2

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -33,8 +33,8 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    key_path = "201720809.pem"
+    key_path = "201920710.pem"
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201720809/ans.json"
+    encrypt_ans_path = "../submission/201920710/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
