b446f66abbafe7111d0887324312744db27b03d7
park jin myeong <pjm2476@ajou.ac.kr>
2021-06-08 02:49:34 -0700

    1

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -37,4 +37,6 @@ if __name__ == "__main__":
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
     encrypt_ans_path = "../submission/201720815/ans.json"
+    encrypt_data(key_path, ans, encrypt_ans_path)
+
 
