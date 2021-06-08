4fe284846620c1d8981fa79b2270c76c0fc9e92d
jaehyunk17 <kjh77k@ajou.ac.kr>
2021-06-08 15:03:13 +0900

    try

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -36,5 +36,5 @@ if __name__ == "__main__":
     key_path = "key.pem"
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201621590/ans.json"
+    encrypt_ans_path = "../submission/201720708/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
