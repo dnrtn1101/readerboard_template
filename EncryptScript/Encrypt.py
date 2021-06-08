88e7749a361ba17da910ba2de5f84f5f284414a8
MinjeongKong <kong1301@ajou.ac.kr>
2021-06-08 17:56:14 +0900

    Minjeong

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -33,7 +33,7 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    key_path = "201720815.pem"
+    key_path = "201823787.pem"
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
     encrypt_ans_path = "../submission/201720815/ans.json"
