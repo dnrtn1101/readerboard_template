8fe6d01946a9b91218ea76a5224a0938b12e117b
유승민 <copomos00@ajou.ac.kr>
2021-06-08 22:45:00 +0900

    201920792

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -33,10 +33,10 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    key_path = "201720815.pem"
+    key_path = "key.pem"
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201720815/ans.json"
+    encrypt_ans_path = "../submission/201920792/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
 
