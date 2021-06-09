7bd9d9502550a4a6f6df22183764ef5e0f3b0aaa
pyj <pyj12711@ajou.ac.kr>
2021-06-09 17:45:17 +0900

    4

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -36,10 +36,10 @@ if __name__ == "__main__":
 
     
 
-    key_path = "201920785.pem"
-    raw_ans_path="ans10.txt"
+    key_path = "201920747.pem"
+    raw_ans_path="ans3.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201920785/ans.json"
+    encrypt_ans_path = "../submission/201920747/ans.json"
 
 
     encrypt_data(key_path, ans, encrypt_ans_path)
