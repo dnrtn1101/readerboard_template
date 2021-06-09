7c43fcd8666877baa3ceaefe0fc1b2f10ea5fee1
julie0005 <julie0005@ajou.ac.kr>
2021-06-09 21:19:50 +0900

    June 9th 1st trial

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -33,9 +33,9 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    key_path = "key.pem"
-    raw_ans_path="../../../HW6/ans.txt"
+    key_path = "201920797.pem"
+    raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201720806/ans.json"
+    encrypt_ans_path = "../submission/201920797/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
