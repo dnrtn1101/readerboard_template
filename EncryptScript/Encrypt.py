39f2ad4087872365428105f3723113f48a7fa688
eter2hy <eter2hy@ajou.ac.kr>
2021-06-11 01:54:32 +0900

    try1

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -35,10 +35,10 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 if __name__ == "__main__":
 
 
-    key_path = "201823776.pem"
+    key_path = "key.pem"
     raw_ans_path = "ans.txt"
     ans = read_txt(raw_ans_path)
 
-    encrypt_ans_path = "../submission/201823776/ans.json"
+    encrypt_ans_path = "../submission/201920704/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
