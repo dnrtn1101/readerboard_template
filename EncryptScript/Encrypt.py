40affdba584e40f717d03a7a482af4a09c8d810a
조형준 <unlimit13@ajou.ac.kr>
2021-06-10 13:00:58 +0900

    1

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -33,9 +33,9 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    key_path = "key.pem"
+    key_path = "201720745.pem"
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201920740/ans.json"
+    encrypt_ans_path = "../submission/201720745/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
