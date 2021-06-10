a749930a0d0a218561c2fbfb466a0fba2385555f
fbwlgh07 <fbwlgh07@gmail.com>
2021-06-11 02:30:10 +0900

    0611

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -36,11 +36,11 @@ if __name__ == "__main__":
 
 
 
-    key_path = "key.pem"
+    key_path = "201720769.pem"
     raw_ans_path = "ans.txt"
 
     ans = read_txt(raw_ans_path)
 
-    encrypt_ans_path = "../submission/201920801/ans.json"
+    encrypt_ans_path = "../submission/201720769/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
