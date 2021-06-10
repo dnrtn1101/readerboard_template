992a4211d54acabb005df701eea083c05fa36b9a
201720812 <vacteria007@ajou.ac.kr>
2021-06-11 07:37:13 +0900

    exp

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -36,11 +36,11 @@ if __name__ == "__main__":
 
 
 
-    key_path = "201920797.pem"
+    key_path = "201720812.pem"
     raw_ans_path = "ans.txt"
 
     ans = read_txt(raw_ans_path)
 
-    encrypt_ans_path = "../submission/201920797/ans.json"
+    encrypt_ans_path = "../submission/201720812/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
