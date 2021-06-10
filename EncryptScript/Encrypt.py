2be8acccbe2ca04a411b73f32bc7894eeb850b21
eter2hy <eter2hy@ajou.ac.kr>
2021-06-11 03:35:15 +0900

    try3

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -36,11 +36,11 @@ if __name__ == "__main__":
 
 
 
-    key_path = "201720769.pem"
+    key_path = "key.pem"
     raw_ans_path = "ans.txt"
 
     ans = read_txt(raw_ans_path)
 
-    encrypt_ans_path = "../submission/201720769/ans.json"
+    encrypt_ans_path = "../submission/201920704/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
