65bd361ad10023698546dbe0cd7e2f33a5545287
ihcac <hanseong0826@ajou.ac.kr>
2021-06-11 01:44:49 +0900

    1

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -35,10 +35,10 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 if __name__ == "__main__":
 
 
-    key_path = "201920785.pem"
-    raw_ans_path = "ans16.txt"
+    key_path = "201823776.pem"
+    raw_ans_path = "ans.txt"
     ans = read_txt(raw_ans_path)
 
-    encrypt_ans_path = "../submission/201920785/ans.json"
+    encrypt_ans_path = "../submission/201823776/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
