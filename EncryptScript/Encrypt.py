1fee318772713916eeeb2b69099405308e181d5d
ohseoha <tnqls8952@ajou.ac.kr>
2021-06-11 16:29:09 +0900

    try6

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -33,10 +33,10 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    key_path = "201920710.pem"
+    key_path = "key.pem"
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201920710/ans.json"
+    encrypt_ans_path = "../submission/201621590/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
 
