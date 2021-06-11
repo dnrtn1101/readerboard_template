76902aefac35be66576abd8ae31f0bc314b4c4ca
Mimiminz <pida6716@ajou.ac.kr>
2021-06-11 22:39:38 +0900

    3

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -33,8 +33,8 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    key_path = "201720707.pem"
+    key_path = "201920721.pem"
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201720707/ans.json"
+    encrypt_ans_path = "../submission/201920721/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
