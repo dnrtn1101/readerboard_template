09a806e80487c7425ec116a8bb387223e25b28fe
park jin myeong <pjm2476@ajou.ac.kr>
2021-06-11 07:44:21 -0700

    retry

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -33,8 +33,10 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    key_path = "201720769.pem"
+    key_path = "201720815.pem"
     raw_ans_path="ans.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201720769/ans.json"
+    encrypt_ans_path = "../submission/201720815/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
+
+
