817bd1a43fcf4eebf66e7f79dcc335ebf95e62d4
Ji-Soo-Kim <chensa1018@ajou.ac.kr>
2021-06-11 18:12:21 +0900

    try2

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -33,9 +33,9 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 
 if __name__ == "__main__":
-    key_path = "201823780.pem"
-    raw_ans_path="ans.txt"
+    key_path = "201920801.pem"
+    raw_ans_path="ans4.txt"
     ans=read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201823780/ans.json"
+    encrypt_ans_path = "../submission/201920801/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
