f248564628ff8f7a2c562b06a1238911476d60c9
hajin <gkwls1130@ajou.ac.kr>
2021-06-11 12:24:20 +0900

    submit 11

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -38,6 +38,6 @@ if __name__ == "__main__":
 
     raw_ans_path = "ans.txt"
     ans = read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201720804/ans.json"
+    encrypt_ans_path = "../submission/201920764/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
