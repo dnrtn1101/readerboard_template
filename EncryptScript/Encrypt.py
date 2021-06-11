de208f10ba990fcd0e9ff02d684576f815fb24fb
Uhyeon Park <warp26@ajou.ac.kr>
2021-06-11 09:59:21 +0900

    흠

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -38,6 +38,6 @@ if __name__ == "__main__":
 
     raw_ans_path = "ans.txt"
     ans = read_txt(raw_ans_path)
-    encrypt_ans_path = "../submission/201620615/ans.json"
+    encrypt_ans_path = "../submission/201920740/ans.json"
     encrypt_data(key_path, ans, encrypt_ans_path)
 
